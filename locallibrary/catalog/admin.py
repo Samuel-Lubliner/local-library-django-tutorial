4737141517014941dbb452d67543bdee044e6d36
Samuel Lubliner <samlubliner@gmail.com>
2024-05-28 19:17:50 +0000

    add inline listing of Book items to the Author detail view

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -4,11 +4,16 @@ from .models import Author, Genre, Book, BookInstance, Language
 admin.site.register(Genre)
 admin.site.register(Language)
 
+class BooksInline(admin.TabularInline):
+    model = Book
+
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
 
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+    inlines = [BooksInline]
 
 admin.site.register(Author, AuthorAdmin)
 
